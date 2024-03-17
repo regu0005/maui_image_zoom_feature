f71424d1c2c8784b52aaf17bac4ba4fa879ef220
gustavoreguerin <regu0005@algonquinlive.com>
2024-03-17 10:44:57 -0400

    Gif added

--- image_popup_and_zoom/MainPage.xaml.cs
+++ image_popup_and_zoom/MainPage.xaml.cs
@@ -2,24 +2,38 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    double currentScale = 1;
+    const double ZoomFactor = 1.2;
 
-	public MainPage()
+    public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    void OnPinchUpdated(object sender, PinchGestureUpdatedEventArgs e)
+    {
+        if (e.Status == GestureStatus.Running)
+        {
+            // Calculate the scale factor to be applied
+            currentScale += (e.Scale - 1) * currentScale;
+            // Prevent scaling to less than the original size
+            currentScale = Math.Max(1, currentScale); 
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+            // Apply the scale factor to the image.
+            ((Image)sender).Scale = currentScale;
+        }
+    }
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    private void OnZoomInClicked(object sender, EventArgs e)
+    {
+        // Increase the scale factor
+        zoomableImage.Scale *= ZoomFactor;
+    }
+
+    private void OnZoomOutClicked(object sender, EventArgs e)
+    {
+        // Decrease the scale factor, but prevent it from becoming too small
+        double newScale = zoomableImage.Scale / ZoomFactor;
+        zoomableImage.Scale = Math.Max(1, newScale);
+    }
 }
-
-
